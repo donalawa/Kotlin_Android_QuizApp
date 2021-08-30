04dfc2bcc7099b7e3cf4ef08ae50b80a7163f144
donalawa <awadonalcien12@gmail.com>
2021-08-29 19:35:25 -0700

    First Activity Complete

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+//        To make sure app takes full screen
+//        WindowCompat.setDecorFitsSystemWindows(window, true)
     }
 }
