63d3462708901332556cbf2255726e9330a63a6f
donalawa <awadonalcien12@gmail.com>
2021-08-30 14:44:04 -0700

    Selected answer completed

--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -1,25 +1,53 @@
 package com.example.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
-class QuizQuestionsActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var mCurrentPosition: Int = 1
+    private var mQuestionsList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition : Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?)  {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
-        val questionsList = Constants.getQuestions()
-        Log.i("Questions Size","${questionsList.size}")
-        Toast.makeText(this,"Size is ${questionsList.size}",Toast.LENGTH_SHORT).show()
+        mQuestionsList = Constants.getQuestions()
+//        Log.i("Questions Size","${questionsList.size}")
+//        Toast.makeText(this,"Size is ${questionsList.size}",Toast.LENGTH_SHORT).show()
 
-        val currentPosition = 1
-        val question: Question? = questionsList[currentPosition - 1]
+        setQuestion()
 
-        progressBar.progress = currentPosition
-        tv_progress.text = "$currentPosition" + "/" + progressBar.max
+        tv_option_one.setOnClickListener(this)
+        tv_option_two.setOnClickListener(this)
+        tv_option_three.setOnClickListener(this)
+        tv_option_four.setOnClickListener(this)
+        btn_submit.setOnClickListener(this)
+    }
+
+    private fun setQuestion() {
+//        mCurrentPosition = 1
+        val question = mQuestionsList!![mCurrentPosition-1]
+//        Each time we set a new question we want to make sure no question is selected so we reset to default view
+        defaultOptionsView()
+
+        if(mCurrentPosition == mQuestionsList!!.size) {
+            btn_submit.text = "FINISH"
+        }else {
+            btn_submit.text = "SUBMIT"
+        }
+
+        progressBar.progress = mCurrentPosition
+        tv_progress.text = "$mCurrentPosition" + "/" + progressBar.max
 
 
         tv_question.text = question!!.question
@@ -28,5 +56,107 @@ class QuizQuestionsActivity : AppCompatActivity() {
         tv_option_two.text = question.optionTwo
         tv_option_three.text = question.optionThree
         tv_option_four.text = question.optionFour
+    }
+
+    private fun defaultOptionsView() {
+        val options = ArrayList<TextView>()
+        options.add(0,tv_option_one)
+        options.add(1,tv_option_two)
+        options.add(2, tv_option_three)
+        options.add(3,tv_option_four)
+
+        for(option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+        }
+    }
+
+//    This Onclick listener is now been reference by the class each time a user clicks on any of the buttons
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.tv_option_one -> {
+                selectedOptionView(tv_option_one,1)
+            }
+
+            R.id.tv_option_two -> {
+                selectedOptionView(tv_option_two, 2)
+            }
+
+            R.id.tv_option_three -> {
+                selectedOptionView(tv_option_three, 3)
+            }
+
+            R.id.tv_option_four -> {
+                selectedOptionView(tv_option_four,4)
+            }
+
+            R.id.btn_submit -> {
+                if(mSelectedOptionPosition == 0) {
+                    mCurrentPosition += 1
+
+                    when{
+                        mCurrentPosition <= mQuestionsList!!.size -> {
+                            setQuestion()
+                        }else -> {
+                            Toast.makeText(this,"You have successfully completed the quiz",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }else {
+                    val question = mQuestionsList?.get(mCurrentPosition - 1)
+                    if(question!!.correctAnswer != mSelectedOptionPosition) {
+                        answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    }
+//                    We highlight the correct answer either ways
+                    answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
+//                    Now we want to change button to Next
+                    if(mCurrentPosition == mQuestionsList!!.size) {
+                         btn_submit.text = "FINISH"
+                    }else {
+                        btn_submit.text = "GO TO NEXT QUESTION"
+                    }
+//                    We need to set our selected position back to 0 in other to proceed
+                    mSelectedOptionPosition = 0
+                }
+            }
+        }
+    }
+
+    private fun answerView(answer: Int, drawableView: Int) {
+            when(answer) {
+                1 -> {
+                    tv_option_one.background = ContextCompat.getDrawable(this, drawableView)
+                }
+
+                2 -> {
+                    tv_option_two.background = ContextCompat.getDrawable(this,drawableView)
+                }
+
+                3 -> {
+                    tv_option_three.background = ContextCompat.getDrawable(this,drawableView)
+                }
+                4 -> {
+                    tv_option_four.background = ContextCompat.getDrawable(this,drawableView)
+                }
+            }
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
+
+        defaultOptionsView()
+//        We need to set the new value for the current selected position
+        mSelectedOptionPosition = selectedOptionNum
+
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.selected_option_border_bg
+        )
+
+
     }
 }
