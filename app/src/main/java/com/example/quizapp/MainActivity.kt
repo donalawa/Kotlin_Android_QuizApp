f7e9edf3e4782414425b2031724a06b6b569db89
donalawa <awadonalcien12@gmail.com>
2021-08-29 21:57:47 -0700

    Getting Question Working

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.view.WindowCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +15,19 @@ class MainActivity : AppCompatActivity() {
 
 //        To make sure app takes full screen
 //        WindowCompat.setDecorFitsSystemWindows(window, true)
+        btn_start.setOnClickListener{
+
+            if(et_name.text.toString().isEmpty()){
+                Toast.makeText(this,"Please Enter Your Name", Toast.LENGTH_SHORT).show()
+            }else {
+//              Moving over to the next activity screen we set an intent the set context to current screen followed by the screen that it will move to
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+//              We want to start the intent activity
+                startActivity(intent)
+//              Then we want to close the current activity because we dont want the user to be able to return here
+                finish()
+            }
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -0,0 +1,32 @@
+package com.example.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
+
+class QuizQuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+
+        val questionsList = Constants.getQuestions()
+        Log.i("Questions Size","${questionsList.size}")
+        Toast.makeText(this,"Size is ${questionsList.size}",Toast.LENGTH_SHORT).show()
+
+        val currentPosition = 1
+        val question: Question? = questionsList[currentPosition - 1]
+
+        progressBar.progress = currentPosition
+        tv_progress.text = "$currentPosition" + "/" + progressBar.max
+
+
+        tv_question.text = question!!.question
+        iv_image.setImageResource(question.image)
+        tv_option_one.text = question.optionOne
+        tv_option_two.text = question.optionTwo
+        tv_option_three.text = question.optionThree
+        tv_option_four.text = question.optionFour
+    }
+}
